2cf3f9eeefa2eb195f070b917eb8f66c45b6b412
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2017-11-28 02:53:51 +0300

    Add homework7 (wip)

---
+++ homework7/src/JoinList.hs
@@ -0,0 +1,110 @@
+module JoinList where
+
+import Sized
+import Data.Monoid ((<>))
+
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+  deriving (Eq, Show)
+
+append :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+append l r = Append ((tag l) <> (tag r)) l r
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) = append
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
+
+(!!?) :: [a] -> Int -> Maybe a
+[]      !!? _         = Nothing
+_       !!? i | i < 0 = Nothing
+(x:xs)  !!? 0         = Just x
+(x:xs)  !!? i         = xs !!? (i - 1)
+
+list :: JoinList m a -> [a]
+list Empty = []
+list (Single _ a) = [a]
+list (Append _ l r) = list l ++ list r
+
+indexJ' :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ' i jl = (list jl) !!? i
+
+-- unbalanced:
+-- ===============================
+--
+--                12
+--         7              5
+--    4         3    4          1
+--    ...........................
+--
+-- balanced:
+-- ===============================
+--
+--                12
+--         5              7
+--    2         3    3         4
+--    ...........................
+--
+--
+--    indexJ:
+--
+--    i = 8
+--    i = 3
+--
+--    sp = sl + sr
+--    i >  sp -> Nothing
+--    i <  sl -> indexJ i l
+--    i >= sl -> indexJ (i - sl) r
+--
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ (Single _ x) = Just x
+indexJ i (Append sp l r) | i < (size' sp) && i >= 0 =
+  case compare i sl of
+    LT -> indexJ i l
+    _ -> indexJ (i - sl) r
+  where sl = sizeJ l
+indexJ _ _ = Nothing
+
+-- drop' :: Int -> [a] -> [a]
+-- drop' n _ | n <= 0 = []
+-- drop' _ [] = []
+-- drop' n (_:xs) = drop (n - 1) xs
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ _ Empty = Empty
+dropJ n l | n <= 0 = l
+dropJ _ (Single _ _) = Empty
+dropJ n (Append _ l r) =
+  let sl = sizeJ l
+      sr = sizeJ r
+  in case compare n sl of
+    LT -> dropJ (n - sr) l
+    _ -> dropJ (n - sl) r
+
+-- take' :: Int -> [a] -> [a]
+-- take' n _ | n <= 0 = []
+-- take' _ [] = []
+-- take' n (x:xs) = x : take' (n - 1) xs
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ _ Empty = Empty
+takeJ n l | n <= 0 = Empty
+takeJ _ l@(Single _ _) = l
+takeJ n (Append _ l r) =
+  case compare n sl of
+    GT -> append l $ takeJ (n - sl) r
+    _ -> takeJ n l
+  where sl = sizeJ l
+
+sizeJ :: (Sized b) => JoinList b a -> Int
+sizeJ Empty = 0
+sizeJ (Single s _) = size' s
+sizeJ (Append s _ _) = size' s
+
+size' :: (Sized b) => b -> Int
+size' = getSize . size
